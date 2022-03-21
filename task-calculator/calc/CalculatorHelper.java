5b4b5f495c31169e59e365c9141ee6ace1c2a2a4
avdavydov <57043729+avdavydov@users.noreply.github.com>
2022-03-21 15:00:56 +0700

    init

---
+++ task-calculator/calc/CalculatorHelper.java
@@ -0,0 +1,45 @@
+package calc;
+
+import java.util.Arrays;
+
+class CalculatorHelper {
+
+    static String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+    };
+
+    static String convertIntegerToRoman(int arabic){
+        final String s = roman[arabic];
+        return s;
+    }
+
+    static int convertRomanToInteger(String romanNumeral){
+        return Arrays.asList(roman).indexOf(romanNumeral);
+    }
+
+     static int do_calc(int number1, int number2, char operation) {
+        int result = 0;
+        switch (operation) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+
+        }
+        return result;
+    }
+
+}
